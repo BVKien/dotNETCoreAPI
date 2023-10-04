35f436f931114867ed487572f6eeb9086406e9e2
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-04 20:05:06 +0700

    Building Api fake database

---
+++ ContactsAPI-master/ContactsAPI/Controllers/ContactsController.cs
@@ -0,0 +1,83 @@
+﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Controllers
+{ 
+    [ApiController]
+    [Route("api/contacts")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+
+              return Ok(await dbContext.Contacts.ToListAsync());
+        }
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult>GetContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            else
+                return Ok(contact);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddContact([FromBody] ContactViewModel addcontactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                FullName = addcontactRequest.FullName,
+                Email = addcontactRequest.Email,
+                Phone = addcontactRequest.Phone,
+                Address = addcontactRequest.Address
+            };
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+            return Ok(contact);
+        }
+       
+        [Route("{id:guid}")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateContact ([FromRoute] Guid id,[FromBody] ContactViewModel contact)
+        {
+            var myContact=await dbContext.Contacts.FindAsync(id);
+            if (myContact != null)
+            {
+                myContact.FullName = contact.FullName;
+                myContact.Email = contact.Email;
+                myContact.Phone = contact.Phone;
+                myContact.Address = contact.Address;
+                await dbContext.SaveChangesAsync();
+                return Ok(myContact);
+            }
+            return NotFound();
+        }
+        [Route("{id:guid}")]
+        [HttpDelete]
+        public async Task<IActionResult> Deletecontact([FromRoute]Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                dbContext.Contacts.Remove(contact);
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ TestApi/TestApi3/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestApiFakeDB.Models;
+
+namespace TestApiFakeDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Kien", Year = 2002},
+            new Product { Id = 2, Name = "Bui", Year = 2002},
+            new Product { Id = 3, Name = "Van", Year = 2002},
+            new Product { Id = 4, Name = "Dep Zai", Year = 2002}
+        };
+
+        [HttpGet("/GetAll")]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(products);
+        }
+
+        // Get/Read
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // Post/Add
+        [HttpPost]
+        public ActionResult<Product> AddProduct(Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            product.Id = products.Count + 1;
+            products.Add(product);
+            return Ok(product);
+        }
+
+        // Put/Update
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Product updateProduct)
+        {
+            var exitingProduct = products.FirstOrDefault(p => p.Id == id);
+
+            if(exitingProduct == null)
+            {
+                return NotFound();
+            }
+
+            exitingProduct.Name = updateProduct.Name;
+            exitingProduct.Year = updateProduct.Year;
+
+            return NoContent();
+        }
+
+        // Delete
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound(product);
+            }
+
+            products.Remove(product);
+
+            return NoContent();
+        }
+
+    }
+}
