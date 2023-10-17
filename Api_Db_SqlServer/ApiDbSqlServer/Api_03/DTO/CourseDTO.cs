0d8b655835aef40c66e176dec752a16f4884b804
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-18 01:56:15 +0700

    Success: http post + Add: DTO, Repositories

---
+++ Api_Db_SqlServer/ApiDbSqlServer/Api_03/DTO/CourseDTO.cs
@@ -0,0 +1,10 @@
+﻿using Api_03.Models;
+namespace Api_03.DTO
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Category { get; set; }
+    }
+}
