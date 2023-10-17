f23075ab97aa94e31379759db96299020364475d
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-17 23:51:48 +0700

    Modify: CourseController, add httppost, put, delete

--- Api_Db_SqlServer/ApiDbSqlServer/Api_03/Controllers/CourseController.cs
+++ Api_Db_SqlServer/ApiDbSqlServer/Api_03/Controllers/CourseController.cs
@@ -16,10 +16,66 @@ namespace Api_03.Controllers
         // http
         // get all 
         [HttpGet]
-        public ActionResult<IEnumerable<Course>> Get() 
+        public ActionResult<IEnumerable<Course>> Get()
         {
             var course = db.Courses.ToList();
             return Ok(course);
         }
+
+        // get by id 
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            // find course by id
+            var course = db.Courses.Find(id);
+            // check null
+            if (course == null) return NotFound();
+            // return 
+            return Ok(course);
+        }
+
+        // delete
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            // find course by id 
+            var course = db.Courses.Find(id);
+            // check null
+            if (course == null) return NotFound(course);
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return NoContent();
+        }
+
+        // update
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Course updateCourse)
+        {
+            var curCourse = db.Courses.Find(id);
+            if (curCourse == null) return NotFound();
+
+            curCourse.Id = updateCourse.Id;
+            curCourse.Name = updateCourse.Name;
+            db.SaveChanges();
+
+            // return 204
+            return NoContent();
+        }
+
+        // add
+        [HttpPost]
+        public ActionResult<Course> Post(Course course)
+        {
+            // check null -> bad request 
+            if(course == null) return BadRequest();
+
+            // add
+            course.Id = db.Courses.Count() + 1;
+            db.Add(course);
+            db.SaveChanges();
+
+            return Ok(course);
+        }
+
     }
 }
