f2042d5f1858fab76a89a260bc3ad6560c2b259b
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-21 09:32:26 +0700

    Modified: DemoWebAPI_03 - CourseController

---
+++ DEMO_Api_MySQL/DemoWebAPI/DemoWebAPI_03/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using DemoWebAPI_03.DTO;
+using DemoWebAPI_03.Models;
+using DemoWebAPI_03.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DemoWebAPI_03.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseRepository repo;
+
+        public CourseController(ICourseRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseDTO>> Get()
+        {
+            var courses = repo.GetAll();
+            return Ok(courses);
+        }
+    }
+}
---
+++ DEMO_Api_MySQL/DemoWebAPI/DemoWebAPI_03/Repositories/Implementations/CourseRepository.cs
@@ -0,0 +1,38 @@
+﻿using DemoWebAPI_03.Models;
+using DemoWebAPI_03.DTO;
+using DemoWebAPI_03.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoWebAPI_03.Repositories.Implementations
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly f8dbContext db;
+
+        public CourseRepository(f8dbContext db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<CourseDTO> GetAll()
+        {
+            var courses = db.Courses.Select(c => new CourseDTO
+            {
+                CourseId = c.CourseId,
+                CourseName = c.CourseName,
+                Description = c.Description,
+                CourseInfo = c.CourseInfo,
+                Image = c.Image,
+                VideoIntro = c.VideoIntro,
+                Fee = c.Fee,
+                Status = c.Status,
+                CategoryCategoryId = c.CategoryCategoryId,
+                UserUserId = c.UserUserId
+            }).ToList();
+
+            return courses;
+        }
+    }
+}
