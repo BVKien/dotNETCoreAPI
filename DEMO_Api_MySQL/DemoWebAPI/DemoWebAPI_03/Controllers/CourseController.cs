e33e5583500bb13484ff1c553625fa8f8ec0475f
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-21 13:28:57 +0700

    Modified: MySQL DemoWebAPI_03 add HttpGetById, Post, Put, Delete, modified Note

--- DEMO_Api_MySQL/DemoWebAPI/DemoWebAPI_03/Controllers/CourseController.cs
+++ DEMO_Api_MySQL/DemoWebAPI/DemoWebAPI_03/Controllers/CourseController.cs
@@ -12,17 +12,62 @@ namespace DemoWebAPI_03.Controllers
     public class CourseController : ControllerBase
     {
         private readonly ICourseRepository repo;
-
         public CourseController(ICourseRepository repo)
         {
             this.repo = repo;
         }
-
+        // http 
+        // get
         [HttpGet]
-        public ActionResult<IEnumerable<CourseDTO>> Get()
+        public ActionResult<IEnumerable<CourseDTO>> GetAllCourses()
         {
-            var courses = repo.GetAll();
+            var courses = repo.GetAllCourses();
             return Ok(courses);
         }
+        // get by id
+        [HttpGet("{id}")]
+        public ActionResult GetCourseById(int id)
+        {
+            var course = repo.GetCourseById(id);
+            return Ok(course);
+        }
+
+        // insert 
+        [HttpPost]
+        public ActionResult InsertCourse([FromBody] CourseDTO courseDTO)
+        {
+            if (courseDTO == null) return BadRequest();
+            repo.InsertCourse(courseDTO);
+            // return Ok(courseDTO); // tra ve 200, cung ok nhung ko tot, ko dc goi la tieu chuan RESTful
+            // tra ve 201 Created – Trả về khi một Resource được tạo thành công.
+            return CreatedAtAction("GetCourseById", new { id = courseDTO.CourseId }, courseDTO);
+        }
+
+        // update
+        [HttpPut("{id}")]
+        public ActionResult UpdateCourse(int id, [FromBody] CourseDTO courseDTO)
+        {
+            // check null -> badrequest
+            if(courseDTO == null || courseDTO.CourseId != id) return BadRequest();
+            // get existing course by id
+            var existingCourse = repo.GetCourseById(id);
+            // check null existing course -> notfound
+            if(existingCourse == null) return NotFound();
+            // update
+            repo.UpdateCourse(courseDTO);
+            // return nocontent
+            return NoContent();
+        }
+
+        // delete 
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCourse(int id)
+        {
+            var course = repo.GetCourseById(id);
+            if(course == null) return NotFound();
+            repo.DeleteCourse(id);
+            return NoContent();
+        }
+
     }
 }
--- DEMO_Api_MySQL/DemoWebAPI/DemoWebAPI_03/Repositories/Implementations/CourseRepository.cs
+++ DEMO_Api_MySQL/DemoWebAPI/DemoWebAPI_03/Repositories/Implementations/CourseRepository.cs
@@ -4,20 +4,22 @@ using DemoWebAPI_03.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DemoWebAPI_03.Repositories.Implementations
 {
     public class CourseRepository : ICourseRepository
     {
         private readonly f8dbContext db;
-
         public CourseRepository(f8dbContext db)
         {
             this.db = db;
         }
 
-        public IEnumerable<CourseDTO> GetAll()
+        // get all courses
+        public IEnumerable<CourseDTO> GetAllCourses()
         {
+            // linq den model moi
             var courses = db.Courses.Select(c => new CourseDTO
             {
                 CourseId = c.CourseId,
@@ -34,5 +36,88 @@ namespace DemoWebAPI_03.Repositories.Implementations
 
             return courses;
         }
+
+        // get course by id
+        public CourseDTO GetCourseById(int id)
+        {
+            var course = db.Courses.FirstOrDefault(c => c.CourseId == id);
+            if (course != null)
+            {
+                return new CourseDTO
+                {
+                    CourseId = course.CourseId,
+                    CourseName = course.CourseName,
+                    Description = course.Description,
+                    CourseInfo = course.CourseInfo,
+                    Image = course.Image,
+                    VideoIntro = course.VideoIntro,
+                    Fee = course.Fee,
+                    Status = course.Status,
+                    CategoryCategoryId = course.CategoryCategoryId,
+                    UserUserId = course.UserUserId
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // insert course
+        public void InsertCourse(CourseDTO courseDTO)
+        {
+            // khoi tao 1 course dto moi
+            var course = new Course
+            {
+                CourseName = courseDTO.CourseName,
+                Description = courseDTO.Description,
+                CourseInfo = courseDTO.CourseInfo,
+                Image = courseDTO.Image,
+                VideoIntro = courseDTO.VideoIntro,
+                Fee = courseDTO.Fee,
+                Status = courseDTO.Status,
+                CategoryCategoryId = courseDTO.CategoryCategoryId,
+                UserUserId = courseDTO.UserUserId
+            };
+
+            db.Courses.Add(course);
+            db.SaveChanges();
+        }
+
+        // update course
+        public void UpdateCourse(CourseDTO courseDTO)
+        {
+            // tim course theo find id
+            var course = db.Courses.FirstOrDefault(c=>c.CourseId == courseDTO.CourseId);
+            // check != null -> update
+            if(course != null)
+            {
+                course.CourseName = courseDTO.CourseName;
+                course.Description = courseDTO.Description;
+                course.CourseInfo = courseDTO.CourseInfo;
+                course.Image = courseDTO.Image;
+                course.VideoIntro = courseDTO.VideoIntro;
+                course.Fee = courseDTO.Fee;
+                course.Status = courseDTO.Status;
+                course.CategoryCategoryId = courseDTO.CategoryCategoryId;
+                course.UserUserId = courseDTO.UserUserId;
+
+                db.SaveChanges();
+            }
+        }
+
+        // delete course 
+        public void DeleteCourse(int CourseId) 
+        {
+            // find course have id
+            var course = db.Courses.FirstOrDefault(c => c.CourseId == CourseId);
+            // check null -> remove
+            if(course != null)
+            {
+                db.Courses.Remove(course);
+                db.SaveChanges() ;
+            }
+        }
+
     }
 }
